8b2ef1b59966fe2d252cbae99229915c661a147c
Saintnb <gonzaleznoeliab@gmail.com>
2021-11-01 09:43:09 -0300

    Command controllers

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 namespace CommandsService.Controllers
 {
@@ -6,9 +10,24 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
+            _repository = repository;
+            _mapper = mapper;
         }
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from CommandService...");
+
+            var platformsItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformsItems));
+        }
+
 
         [HttpPost]
         public ActionResult TestInBoundConnection()
